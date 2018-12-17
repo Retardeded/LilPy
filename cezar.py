96f58bb0151b7137001e3299206791c37c34ceeb
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2018-12-17 15:17:19 +0100

    Add files via upload

---
+++ cezar.py
@@ -0,0 +1,51 @@
+from random import randint
+from math import *
+
+def remove_space(text):
+  tmp = []
+  to_low = 20
+  for i in range(0, len(text)):
+    print(ord(text[i]))
+
+    if(ord(text[i]) != 32 ):
+      if(ord(text[i]) > 90):
+        letter = chr(ord(text[i]) - to_low)
+        tmp.append(letter)
+      else:
+        tmp.append(text[i])
+
+  return tmp
+
+def encrypt_cezar(text, value):
+  for i in range(0, len(text)):
+    letter_encoded = ord(text[i])
+    letter_encoded = 97 + (letter_encoded-97 + value ) % 26
+    text[i] = chr(letter_encoded)
+
+def decrypt_cezar(text, value):
+  for i in range(0, len(text)):
+    letter_encoded = ord(text[i])
+    letter_encoded = 97 + (letter_encoded-97 - value) % 26
+    text[i] = chr(letter_encoded)
+
+text = "teskt do zaszyfrowania"
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+alphabet_chars = []
+for i in range(0, len(alphabet_chars)):
+  alphabet_chars.insert(0, alphabet[i])
+
+chars = []
+for i in range(0, len(text)):
+  chars.append(text[i])
+
+value_to_sub = 97
+random_value = 2
+
+print(chars)
+char = remove_space(chars)
+print(chars)
+encrypt_cezar(chars, random_value)
+print(chars)
+decrypt_cezar(chars, random_value)
+print(chars)
