8b0684e0903988584f010b20b7a6686d926fbd99
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2019-01-05 17:51:25 +0100

    Delete to_2_digits.py

--- scratches/to_2_digits.py
+++
@@ -1,9 +0,0 @@
-print("Enter number in base 2")
-
-number = str(input())
-number_base_ten = 0
-for i in range(0, len(number)):
-    number_base_ten *= 2
-    number_base_ten += int(number[i])
-
-print("number: "+ str(number) + "(2) in base 10 is " + str(number_base_ten))
